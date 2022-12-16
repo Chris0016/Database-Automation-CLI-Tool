70419c99ba7e96a2362c8798ff0c50b0f4c34917
chris0016 <cper3745@lehmanhs.com>
2022-12-16 12:58:17 -0500

    Load DB Configs from Properties File

--- src/main/java/com/dbautomation/database/DBConnection.java
+++ src/main/java/com/dbautomation/database/DBConnection.java
@@ -1,28 +1,43 @@
 package com.dbautomation.database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-// TODO: Import url from properties file
+import java.util.Properties;
 
 public class DBConnection implements AutoCloseable {
-    private String url = "jdbc:mysql://" + DBConfigs.dbhost + ":" + DBConfigs.dbport + "/" + DBConfigs.dbschema
-            + "?useTimezone=true&serverTimezone=UTC";;
+
+    final String PATH_TO_CONFIG = "dbautomationtool/resources/config.properties";
+
+    private Properties prop;
 
     private Connection connection;
     private Statement stmt;
     private PreparedStatement p;
 
-    public DBConnection() throws SQLException, ClassNotFoundException {
-        try {
+    public DBConnection() throws SQLException, ClassNotFoundException, IOException {
+
+        try (InputStream input = new FileInputStream(PATH_TO_CONFIG)) {
+            prop = new Properties();
+
+            prop.load(input);
+
+            System.out.println(
+                    prop.getProperty("db.host")
+                            + "\n" + prop.getProperty("db.port"));
+
+            String url = "jdbc:mysql://" + prop.getProperty("db.host") + ":" + prop.getProperty("db.port") + "/"
+                    + prop.getProperty("db.schema") + "?useTimezone=true&serverTimezone=UTC";
+
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, DBConfigs.dbusername, DBConfigs.dbpassword);
+            connection = DriverManager.getConnection(url, prop.getProperty("db.user"), prop.getProperty("db.password"));
             stmt = connection.createStatement();
             p = connection.prepareStatement("hello");
-
         } finally {
             if (connection == null)
                 throw new SQLException("Unable to create connection with database");
